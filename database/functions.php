a0fecc3a6544923d67ae92f9b9a3feae68cbfb1b
teeratouch <84368651+teeratouch@users.noreply.github.com>
2021-05-18 15:43:09 +0700

    Create functions.php

---
+++ database/functions.php
@@ -0,0 +1,22 @@
+<?php
+	
+	function clean($data)
+	{
+		$data = trim($data);
+		$data = stripcslashes($data);
+		
+		return $data;
+		  
+	}
+	
+	function showPrompt()
+	{
+		echo "<div class='alert alert-success'>".$_SESSION['prompt']."</div>";
+	}
+	
+	function showError()
+	{
+		echo "<div class='alert alert-danger'>".$_SESSION['errprompt']."</div>";
+	}
+	
+?>
